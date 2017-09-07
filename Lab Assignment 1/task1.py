b59a68c5862fb553659e064c2c168d1178e29cf9
aadi22796 <31552848+aadi22796@users.noreply.github.com>
2017-09-07 15:40:46 -0500

    Update task1.py

--- Lab Assignment 1/task1.py
+++ Lab Assignment 1/task1.py
@@ -1,11 +1,11 @@
 fp=open("para.txt","r")
-fi=open("inst.txt","w+")
-wlist2=list()
-wlist=fp.read().split(" ")
-for i in range(0,len(wlist)):
-    wlist2.append("0")
+fi=open("inst.txt","w+")    #opens input and output files respectively
+wlist2=list()               #initializes list
+wlist=fp.read().split(" ")  #splits entire paragraphs into words and pushes them into list
+for i in range(0,len(wlist)):   
+    wlist2.append("0")          #appends a 0 to wlist2 for every occurence of wlist
     for j in range(0, len(wlist)):
-        if (wlist[i] == wlist[j]):
-            wlist2[i]= int(wlist2[i])+1
+        if (wlist[i] == wlist[j]):  
+            wlist2[i]= int(wlist2[i])+1     #Adds 1 to corresponding wlsit2 value of wlist1, if there is a word occurence for another time
 for l in range(0,len(wlist)):
-    fi.write(wlist[l]+" ---> "+str(wlist2[l])+"\n")
+    fi.write(wlist[l]+" ---> "+str(wlist2[l])+"\n") #Writes the output to the output file
