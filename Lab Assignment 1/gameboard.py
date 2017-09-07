24820cb06112ac84ae1ba9a4328aee0ae9d27156
aadi22796 <31552848+aadi22796@users.noreply.github.com>
2017-09-07 15:27:56 -0500

    Update gameboard.py

--- Lab Assignment 1/gameboard.py
+++ Lab Assignment 1/gameboard.py
@@ -1,12 +1,10 @@
-hinp=int(input("Enter Height of Board : "))
-winp=int(input("Enter Width of Board : "))
+hinp=int(input("Enter Height of Board : "))     
+winp=int(input("Enter Width of Board : "))      #For taking input height and input width respectively
 
-def board_draw(height,width):
-    i=0
-
-    for j in range(0,height):
-        print(" ---  "*width)
+def board_draw(height,width):                   #Initializing a drawing function
+    for j in range(0,height):                   #In this loop, the reverse shaped 'U's are drawn which contains - and |, according to count given
+        print(" ---  "*width) 
         print("|   | "*width)
-    print(" ---  "*width)
+    print(" ---  "*width)                       # prints the last line of '---'
 
-board_draw(hinp,winp)
+board_draw(hinp,winp)                           #draws board using the function
