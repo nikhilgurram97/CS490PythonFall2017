326c43bcbe9d2925d7c353cbbeaf7e6db6c7bfcd
aadi22796 <31552848+aadi22796@users.noreply.github.com>
2017-09-07 15:37:35 -0500

    Update task1a.py

--- Lab Assignment 1/task1a.py
+++ Lab Assignment 1/task1a.py
@@ -1,9 +1,9 @@
 fp=open("para.txt","r")
-fi=open("insta.txt","w+")
-wset={}
-for a in fp.read().split(" "):
+fi=open("insta.txt","w+")   #opens input and output files respectively
+wset={}                        #an empty set initialization
+for a in fp.read().split(" "):  #splits the entire paragraphs into words and pushes the words into sets
     if a not in wset:
-        wset[a]=1
+        wset[a]=1               #Values change to 1
     else:
-        wset[a]+=1
-fi.write(str(wset))
+        wset[a]+=1              #if already existing, 1 value is added
+fi.write(str(wset))             #finally, set is written to file
