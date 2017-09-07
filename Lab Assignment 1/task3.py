51d94427826477d4aef55d38b00ecf0309632157
aadi22796 <31552848+aadi22796@users.noreply.github.com>
2017-09-07 15:29:16 -0500

    Update task3.py

--- Lab Assignment 1/task3.py
+++ Lab Assignment 1/task3.py
@@ -1,4 +1,4 @@
 for i in range(700,1700):   #initializing loop
     if(i%5==0):             #Divisible by 5 validation
-        if(i%2==0):         #Divisible by 2 validation
+        if(i%2==0):         #Multiple of 2 validation
             print(i)        #print number
